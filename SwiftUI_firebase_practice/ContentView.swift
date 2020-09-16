0b1bb57ba4514ef7930e6d9c2e5df866f415cfe8
forestkoju <forestkoju0514@gmail.com>
2020-09-16 13:00:07 +0900

    Initial Commit

---
+++ SwiftUI_firebase_practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_firebase_practice
+//
+//  Created by forestkoju on 2020/09/16.
+//  Copyright © 2020 forestkoju. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
