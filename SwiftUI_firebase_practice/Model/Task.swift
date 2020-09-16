de3a903ecd4cc0fbd30c6e868aa5a905fda70477
forestkoju <forestkoju0514@gmail.com>
2020-09-16 13:26:31 +0900

    organized files

---
+++ SwiftUI_firebase_practice/Model/Task.swift
@@ -0,0 +1,24 @@
+//
+//  Task.swift
+//  SwiftUI_firebase_practice
+//
+//  Created by forestkoju on 2020/09/16.
+//  Copyright © 2020 forestkoju. All rights reserved.
+//
+
+import Foundation
+
+
+struct Task{
+    var title: String
+    var completed: Bool
+}
+
+#if DEBUG
+let testDataTasks = [
+    Task(title: "Implement the UI", completed: true),
+    Task(title: "Plan a develpment plan", completed: false),
+    Task(title: "Connect to Firebase", completed: false),
+    Task(title: "Run Test", completed: false)
+]
+#endif
